b1cd67ce45f22bfe6f95ccf14b8559b35552cddf
Manoj <manojkarthick1706@gmail.com>
2024-03-05 10:14:53 +0530

    Created initial layout page and added font for website

--- itdude-ui/src/App.vue
+++ itdude-ui/src/App.vue
@@ -1,10 +1,7 @@
 <script setup></script>
 
 <template>
-  <div class="text-3xl font-bold underline">
-    Hello IT Dood, I have created vue js project and installed tailwind. Good to
-    flyyy...
-  </div>
+  <RouterView />
 </template>
 
 <style></style>
---
+++ itdude-ui/src/views/LayoutView.vue
@@ -0,0 +1,22 @@
+<script></script>
+
+<template>
+  <div class="bg-slate-100 p-3 flex h-lvh">
+    <div class="min-w-52 pr-2">
+      <div
+        class="font-semibold text-xl text-gray-900 hover:bg-gray-100 hover:text-blue-600 rounded-lg px-2 py-1.5 w-auto hover:cursor-pointer"
+      >
+        Image goes here
+      </div>
+    </div>
+    <div class="border-slate-200 border w-full rounded-lg p-4 bg-white">
+      <h3
+        class="font-semibold text-xl text-gray-900 hover:bg-gray-100 hover:text-blue-600 rounded-lg px-2 py-1.5 w-auto cursor-pointer"
+      >
+        Dashboard
+      </h3>
+    </div>
+  </div>
+</template>
+
+<style></style>
